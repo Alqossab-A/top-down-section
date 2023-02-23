8b672ce708684f192d0934d1b2a0e0a978140506
Ahmed Al-Qossab <alqossaba@gmail.com>
2023-02-23 01:46:28 -0500

    Header files and Constructors

---
+++ Character.cpp
@@ -0,0 +1,55 @@
+#include "Character.h"
+#include "raymath.h"
+
+
+Character::Character()
+{
+    width = texture.width/maxFrames;
+    height = texture.height;
+}
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {
+        (float)winWidth / 2.0f - 4.0f * (0.5f * width),
+        (float)winHeight / 2.0f - 4.0f * (0.5f * height)};
+}
+
+void Character::tick(float deltaTime)
+{
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= speed;
+    if (IsKeyDown(KEY_D))
+        direction.x += speed;
+    if (IsKeyDown(KEY_W))
+        direction.y -= speed;
+    if (IsKeyDown(KEY_S))
+        direction.y += speed;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set worldPos = worldPos + direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+    // animation update time
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw the character
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f * width, 4.0f * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
