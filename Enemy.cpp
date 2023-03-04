b0641498172e883f91311ea97837856a74ca2e7b
Ahmed Al-Qossab <alqossaba@gmail.com>
2023-03-03 23:34:00 -0500

    Enemy AI

--- Enemy.cpp
+++ Enemy.cpp
@@ -10,10 +10,20 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
 
     width = texture.width / maxFrames;
     height = texture.height;
+    speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime)
-{
-    screenPos = Vector2Subtract( worldPos, target->getWorldPos());
-   BaseCharacter::tick(deltaTime);
+{   
+    // get toTarget
+    Vector2 toTarget = Vector2Subtract(target->getScreenPos(), screenPos);
+    // normalize toTarget
+    toTarget = Vector2Normalize(toTarget);
+    // multiply toTarget by speed
+    toTarget = Vector2Scale(toTarget, speed);
+    // move Enemy (set worldPos)
+    worldPos = Vector2Add(worldPos, toTarget);
+
+    screenPos = Vector2Subtract(worldPos, target->getWorldPos());
+    BaseCharacter::tick(deltaTime);
 }
